7475134bd3a7e86d4e8f514a1664a4bbdcd4722f
vitaliytv <vitaliytv@nitralabs.com>
2021-03-13 06:34:43 +0200

    wiht backup

--- .env-cmdrc.cjs
+++ .env-cmdrc.cjs
@@ -1,23 +1,26 @@
-const fs = require("fs");
+const fs = require('fs')
 
-module.exports = new Promise((resolve) => {
-  let secret = {};
-  if (fs.existsSync("./.secret.json")) {
-    secret = require("./.secret.json");
+module.exports = new Promise(resolve => {
+  let secret = {}
+  if (fs.existsSync('./.secret.json')) {
+    secret = require('./.secret.json')
   }
 
   resolve({
     development: {
-      NODE_ENV: "development",
-      BUCKET: "tmp-30",
-      TEMPOFILE: 'index.js'
+      NODE_ENV: 'development',
+      BUCKET: 'tmp-30',
+      DB_HOST: 'mssql',
+      DB_NAME: 'b2b',
+      DB_USER: 'sa',
+      DB_PASS: 'cPassword1'
     },
-    test: { NODE_ENV: "test" },
+    test: { NODE_ENV: 'test' },
     production: {
       ...secret,
       ...{
-        NODE_ENV: "production",
-      },
-    },
-  });
-});
+        NODE_ENV: 'production'
+      }
+    }
+  })
+})
--- index.js
+++ index.js
@@ -1,15 +1,31 @@
-// Imports the Google Cloud client library
-import { Storage } from '@google-cloud/storage'
 import fs from 'fs'
 import zlib from 'zlib'
+import { exec } from 'child_process'
+import checkEnvLib from '@47ng/check-env'
+import { Storage } from '@google-cloud/storage'
 
-// Creates a client
+checkEnvLib.default({ required: ['DB_HOST', 'DB_NAME', 'DB_USER', 'DB_PASS'] })
+
+// Imports the Google Cloud client library
 const storage = new Storage()
 
 ;(async () => {
   try {
-    // await gzip(process.env.TEMPOFILE)
-    // await storage.bucket(process.env.BUCKET).upload(process.env.TEMPOFILE, {})
+    const file = `${process.env.DB_NAME}-${new Date()
+      .toISOString()
+      .replace(/\D/g, '-')
+      .slice(0, -5)}.bak`
+    console.log('file: ', file)
+
+    const result = await execPromise(
+      `sqlcmd -S ${process.env.DB_HOST} -U ${process.env.DB_USER} -P ${process.env.DB_PASS} -Q "BACKUP DATABASE ${process.env.DB_NAME} TO DISK='./${file}'"`
+    )
+    console.log('stdout: ', result)
+
+    await gzip(file)
+    console.log('gziped')
+
+    await storage.bucket(process.env.BUCKET).upload(`${file}.gz`, {})
 
     console.log(`copied to bucket`)
   } catch (e) {
@@ -26,8 +42,22 @@ async function gzip(file) {
       .pipe(zip)
       .pipe(writeStream)
       .on('finish', err => {
-        if (err) return reject(err)
-        else resolve()
+        if (err) {
+          return reject(err)
+        }
+        resolve()
       })
+  })
+}
+
+async function execPromise(command) {
+  return new Promise(function (resolve, reject) {
+    exec(command, (error, stdout, stderr) => {
+      if (error) {
+        return reject(error)
+      }
+
+      resolve(stdout.trim())
+    })
   })
 }
