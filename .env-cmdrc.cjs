cd835d0d92bd5882cd42c6c498db7024744ea261
litsovyu <82654310+litsovyu@users.noreply.github.com>
2021-09-16 07:19:28 +0300

    add new host env

--- .env-cmdrc.cjs
+++ .env-cmdrc.cjs
@@ -13,7 +13,15 @@ module.exports = new Promise(resolve => {
       DB_HOST: 'mssql',
       DB_NAME: 'b2b',
       DB_USER: 'sa',
-      DB_PASS: 'cPassword1'
+      DB_PASS: 'cPassword1',
+      DB_HOST_DEST: 'uat',
+      DB_NAME_DEST: 'b2b',
+      DB_USER_DEST: 'sa',
+      DB_PASS_DEST: 'cPassword1',
+      DUMP_PATH_DEST: '/var/opt/mssql/'
+
+
+
     },
     test: { NODE_ENV: 'test' },
     production: {
